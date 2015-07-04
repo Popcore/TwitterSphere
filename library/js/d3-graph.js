50fb8cdac295b052c9c8ad550a95ab8fdfc18d2a
Popcore <smallzed@gmail.com>
2015-07-04 18:59:39 +0100

    added D3 library. Implemented basic pie mood chart

---
+++ library/js/d3-graph.js
@@ -0,0 +1,39 @@
+(function() {
+
+	var dataset = [
+  	{ label: 'Betelgeuse', count: 30 },
+  	{ label: 'Cantaloupe', count: 30 },
+  	{ label: 'Dijkstra', count: 40 }
+	];
+
+	var color = d3.scale.ordinal()
+  .range(['#A60F2B', '#648C85', '#B3F2C9', '#528C18', '#C3F25C']); 
+
+	var svg = d3.select('#mood-info')
+		.append('svg')
+		.attr('width', 200)
+		.attr('height', 200)
+		.append('g')
+		.attr('transform', 'translate(' + 100 +  ',' + 100 + ')');
+
+	var arc = d3.svg.arc()
+		.innerRadius(40)
+		.outerRadius(100);
+
+	var pie = d3.layout.pie()
+		.value(function(d) { return d.count; })
+		.sort(null);
+
+	var path = svg.selectAll('path')
+		.data(pie(dataset))
+		.enter()
+		.append('path')
+		.attr('d', arc)
+		.attr('fill', function(d, i) {
+			return color(d.data.label);
+		});
+
+}()) 
+
+
+ 
